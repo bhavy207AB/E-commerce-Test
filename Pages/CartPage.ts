3cbdca54fa8864160f3ed5a50c3dfc6712cd4811
bhavy207AB <bhavy.alphabin@gmail.com>
2026-08-13 16:44:21 +0530

    re-run testdino workflow

--- Pages/CartPage.ts
+++ Pages/CartPage.ts
@@ -2,7 +2,7 @@ import { expect, Page } from '@playwright/test';
 
 export class CartPage {
   constructor(private page: Page) {}
-//
+//test
   async openCart() {
     await this.page.getByRole('link', {
       name:'Cart'
